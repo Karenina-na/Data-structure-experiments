140190a9083b784f0aa1c555edd1d60d46fc9467
Karenina-na <weizixiang0@outlook.com>
2023-04-10 22:58:19 +0800

    Init

---
+++ cmake-build-debug/CMakeFiles/Data_structure_experiments.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Data_structure_experiments.dir/main.cpp.obj"
+  "CMakeFiles/Data_structure_experiments.dir/main.cpp.obj.d"
+  "Data_structure_experiments.exe"
+  "Data_structure_experiments.exe.manifest"
+  "Data_structure_experiments.pdb"
+  "libData_structure_experiments.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Data_structure_experiments.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
